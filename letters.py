2c1f486f7ea1c7bdf996938b17c7b2cdd98fe84f
Steven Schmatz <stevenschmatz@gmail.com>
2014-03-07 20:46:43 -0500

    lol it works

--- letters.py
+++ letters.py
@@ -19,14 +19,21 @@ class Markov(object):
             else:
                 self.cache[first_two].append(self.text[char + 2])
 
-    def make_text(self):
+    def make_text(self, length=100):
         initial_letters = ''
         while True:
             initial_letters = random.choice(self.cache.keys())
             if (initial_letters[0] == string.upper(initial_letters[0])) and (initial_letters[0].isalnum()):
                 break
-        print initial_letters, random.choice(self.cache[initial_letters])
+        self.new_words = [initial_letters[0], initial_letters[1], random.choice(self.cache[initial_letters])]
+        for i in xrange(length):
+            num_letters = len(self.new_words)
+            self.new_words.append(
+                random.choice(self.cache[(self.new_words[num_letters - 2], self.new_words[num_letters - 1])]))
+
+        return ''.join(self.new_words)
+
 
 my_chain = Markov(my_path)
 my_chain.get_triples()
-my_chain.make_text()
+print my_chain.make_text()
